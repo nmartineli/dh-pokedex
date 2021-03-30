abcf0cfa8fca4fe08071cffeef224f338a5cfa57
nmartineli <nataliamartineli@gmail.com>
2021-03-29 22:09:20 -0300

    Add MVC

--- pokedexMVC/controllers/LegendariesController.js
+++ pokedexMVC/controllers/LegendariesController.js
@@ -1,6 +1,9 @@
+const LegendaryModel = require('../models/LegendaryModel');
+
 const controller = {
 	index: (req, res) => {
-		res.send('ok');
+		const pineco = new LegendaryModel(1, 'Pineco', 'Melhor Pokemon do Mundo', 'Bug');
+		res.json(pineco);
 	},
 };
 
